845eaa3cdd8e96a93fccadf8d58e0bc7bb822fa0
Neema Yousefi <neema@yousefi.dev>
2021-04-05 22:59:51 -0700

    basic install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+ln -sv ~/.dotfiles/vimrc ~/.vimrc
+ln -sv ~/.dotfiles/vim ~/.vim
+ln -sv ~/.dotfiles/zshrc ~/.zshrc
+ln -sv ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sv ~/.dotfiles/git/gitconfig ~/.gitconfig
