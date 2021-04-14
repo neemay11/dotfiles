c55dda2150080167822772042cdcfa6909f305f2
Neema Yousefi <neema@yousefi.dev>
2021-04-13 22:07:58 -0700

    move oh-my-zsh config to seprate file

---
+++ zsh/oh-my-zsh.zsh
@@ -0,0 +1,13 @@
+# oh-my-zsh path
+export ZSH="$DOTFILES/.oh-my-zsh"
+
+# set powerlevel10k as zsh theme
+ZSH_THEME="powerlevel10k/powerlevel10k"
+
+# case sensetive autocomplete
+CASE_SENSITIVE="false"
+
+# oh-my-zsh plugins
+plugins=(git bundler)
+
+source $ZSH/oh-my-zsh.sh
