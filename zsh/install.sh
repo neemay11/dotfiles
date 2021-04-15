a37ab25f1cb5c9c3bdab3098573ddf74bcc4810a
Neema Yousefi <neema@yousefi.dev>
2021-04-14 21:57:10 -0700

    fix zsh install, symlink zshrc after installing oh-my-zsh
    this is to resolve issue where zsh install will use its template zshrc
    file

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,10 +1,13 @@
 #! /bin/sh
 
+echo "Installing ohmyzsh."
+ZSH=$HOME/.dotfiles/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# remove auto-generated .zshrc
+rm -f $HOME/.zshrc
+rm -f $HOME/.zshrc.pre-oh-my-zsh
+
 echo "Linking zsh config."
 ln -sv $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
-
-echo "Installing ohmyzsh."
-ZSH=$HOME/.dotfiles/oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 echo "ZSH setup complete!"
