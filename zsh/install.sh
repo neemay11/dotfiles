b57c23bd6dfb90ee3fef0597f3952a4227014aef
Neema Yousefi <neema@yousefi.dev>
2021-04-14 22:14:16 -0700

    add installation of powerlevel10k theme to zsh install

--- zsh/install.sh
+++ zsh/install.sh
@@ -7,6 +7,9 @@ ZSH=$HOME/.dotfiles/.oh-my-zsh sh -c "$(curl -fsSL https://raw.githubusercontent
 rm -f $HOME/.zshrc
 rm -f $HOME/.zshrc.pre-oh-my-zsh
 
+echo "Installing Powerlevel10k oh-my-zsh theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.dotfiles/.oh-my-zsh/custom}/themes/powerlevel10k
+
 echo "Linking zsh config."
 ln -sv $HOME/.dotfiles/zsh/zshrc $HOME/.zshrc
 
