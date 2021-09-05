d5c97b5cc4955243932b6bad042c6f14cc07a217
Neema Yousefi <neema@yousefi.dev>
2021-09-04 19:38:22 -0700

    misc changes

---
+++ vim/ftplugin/json.vim
@@ -0,0 +1,4 @@
+let b:ale_fixers = ['prettier', 'eslint']
+let b:ale_fix_on_save = 1
+
+call Spaces(2)
--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -26,14 +26,7 @@ eval "$(rbenv init -)"
 # nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-
-# okta
-if [[ -f "$HOME/.okta/bash_functions" ]]; then
-    . "$HOME/.okta/bash_functions"
-fi
-if [[ -d "$HOME/.okta/bin" && ":$PATH:" != *":$HOME/.okta/bin:"* ]]; then
-    PATH="$HOME/.okta/bin:$PATH"
-fi
+source /usr/share/nvm/init-nvm.sh
 
 # Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
@@ -41,3 +34,6 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+# vi mode
+bindkey -v
