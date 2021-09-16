60a55e7fdf819c6cf123247694e32db67a613dd9
Neema Yousefi <nyousefi@inspirecleanenergy.com>
2021-09-16 12:39:40 -0700

    add okta

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -28,6 +28,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
 source /usr/share/nvm/init-nvm.sh
 
+# okta
+if [[ -f "$HOME/.okta/bash_functions" ]]; then
+    . "$HOME/.okta/bash_functions"
+fi
+if [[ -d "$HOME/.okta/bin" && ":$PATH:" != *":$HOME/.okta/bin:"* ]]; then
+    PATH="$HOME/.okta/bin:$PATH"
+fi
+
 # Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
