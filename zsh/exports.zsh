85f9c6a26a66e5303b0bb19f48a5592eb080462a
Neema Yousefi <nyousefi@inspirecleanenergy.com>
2022-05-20 14:03:02 -0700

    changes from new computer

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -23,19 +23,6 @@ export CARGO_HOME="$PROJECTS/.cargo"
 # ruby
 eval "$(rbenv init -)"
 
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-source /usr/share/nvm/init-nvm.sh
-
-# okta
-if [[ -f "$HOME/.okta/bash_functions" ]]; then
-    . "$HOME/.okta/bash_functions"
-fi
-if [[ -d "$HOME/.okta/bin" && ":$PATH:" != *":$HOME/.okta/bin:"* ]]; then
-    PATH="$HOME/.okta/bin:$PATH"
-fi
-
 # Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
@@ -45,3 +32,6 @@ export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # vi mode
 bindkey -v
+
+# gpg
+export GPG_TTY=$(tty)
