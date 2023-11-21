821565b52670219833f8d9ddcef3f42b12e6fec6
Neema Yousefi <neema@yousefi.dev>
2023-11-21 13:45:53 -0800

    correct nvm install location

--- zsh/configs.zsh
+++ zsh/configs.zsh
@@ -28,8 +28,7 @@ eval "$(rbenv init -)"
 
 # nvm
 export NVM_DIR="$HOME/.nvm"
-[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"
-source /usr/share/nvm/init-nvm.sh
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 # Android Studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
