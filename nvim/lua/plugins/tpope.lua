da2520bd4142823617b85d2b76a873a93c789cb8
Neema Yousefi <neema@yousefi.dev>
2026-03-31 16:15:52 -0700

    fugitive keybindings

--- nvim/lua/plugins/tpope.lua
+++ nvim/lua/plugins/tpope.lua
@@ -1,6 +1,13 @@
 return {
-	'tpope/vim-surround',
-	'tpope/vim-fugitive',
-	'tpope/vim-commentary',
-	'tpope/vim-vinegar',
+	"tpope/vim-surround",
+	{
+		"tpope/vim-fugitive",
+		keys = {
+			{ "<leader>gs", ":Git<CR>" },
+			{ "<leader>gd", ":Git diff<CR>" },
+			{ "<leader>gb", ":Git blame<CR>" },
+		},
+	},
+	"tpope/vim-commentary",
+	"tpope/vim-vinegar",
 }
