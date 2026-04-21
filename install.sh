6588f3f1203cbe88549b30257fa1ba871010af15
Neema Yousefi <neema@yousefi.dev>
2026-04-21 14:53:05 -0700

    create path for symlink

--- install.sh
+++ install.sh
@@ -4,11 +4,13 @@ export DOTFILES="$HOME/.dotfiles"
 
 # function to create a symbolic link
 # if the target file already exists, it will be backed up to a .bak file
+# if the path to the target file does not exist, it will be created
 createSymlink() {
 	if [ -e "$1" ]; then
 		echo "$1 already exists, moving to $1.bak"
 		mv "$1" "$1.bak"
 	fi
+	mkdir -p "$(dirname "$1")"
 	ln -sv "$2" "$1"
 }
 
